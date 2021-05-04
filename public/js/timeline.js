3636a69dc5bc4044dc414b06aa6c1bc7284aaf1e
BartMoura <bart.emf@hotmail.com>
2021-05-04 09:19:55 -0300

    refatorando form de cadastrar livro

--- public/js/timeline.js
+++ public/js/timeline.js
@@ -10,17 +10,53 @@ const btnClose = () => {
   document.querySelector('.modal').style.display = "none";
 }
 
-const modal = document.querySelector('.modal');
+// const modal = document.querySelector('.modal');
 
 const btn = document.querySelector('.cadastrar-book');
+// const btnCriar = document.querySelector('.btn-criar');
 
-btn.onclick = function() {
-  modal.style.display = "flex";
+btn.onclick = () => {
+  document.querySelector('.modal').style.display = "flex";
 }
 
-document.querySelector('.cadastrar-book').addEventListener('click', function () {
-  document.querySelector('.model').classList.add('flex');
-});
+const form_submit = () => {
+  document.getElementById("form-create-book").submit();
+} 
+
+// btnCriar.onclick = () => {
+//   document.querySelector("#btn-submit-book");
+// }
+
+
+
+
+// let formContact = document.querySelector('#form_contact');
+
+// formContact.addEventListener("submit", (event) => {
+    // interrompo o envio do formlário
+    // event.preventDefault();
+
+    // validar se todos os campos estão preenchido
+    // validar se nome tem 2 ou mais caracteres
+    // validar se telefone tem no minimo 8 caracteres
+    // validar se o campo email tem @ (Google - validação email com regex)
+    // let nomeValue = document.querySelector("#input_nome").value;
+    // let emailValue = document.querySelector("#input_email");
+    // let telefoneValue = document.querySelector("#input_telefone").value;
+    // let mensagemValue = document.querySelector("#input_mensagem").value;
+//     document.querySelector("#input-name").value
+//     document.querySelector("#input-isbn").value
+//     document.querySelector("#input-pc").value
+//     document.querySelector("#input-writer").value
+//     document.querySelector("#input-genre").value
+//     document.querySelector("#input-year").value
+//     document.querySelector("#input-img").value
+//      formContact.submit();
+
+//  })
+// document.querySelector('.cadastrar-book').addEventListener('click', function () {
+//   document.querySelector('.modal').classList.add('flex');
+// });
 
 // setTimeout( () => {
 //   document.querySelector('.modal').style.display = "flex";
