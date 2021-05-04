9b5fea64e34040bb77c9b7811ff0b819474217d9
BrenaM <brenamonteiro_@hotmail.com>
2021-05-04 13:04:56 -0300

    add modal para notebooks

--- public/js/books.js
+++ public/js/books.js
@@ -6,4 +6,22 @@ $('.container-carousel').slick({
     slidesToScroll: 1,
     adaptiveHeight: true
   });
+
+  const btnClose = () => {
+    document.querySelector('.modal').style.display = "none";
+  }
+  
+  // const modal = document.querySelector('.modal');
+  
+  const btn = document.querySelector('.adicionar-livro');
+  // const btnCriar = document.querySelector('.btn-criar');
+  
+  btn.onclick = () => {
+    document.querySelector('.modal').style.display = "flex";
+  }
+  
+  const form_submit = () => {
+    document.getElementById("form-create-book").submit();
+  } 
+  
  
